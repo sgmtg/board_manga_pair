541abaa72805f33442a231f96bb391e9d559c2db
Gen Sugimoto <shanbenyan3@gmail.com>
2023-05-02 23:10:02 +0900

    [add] successfully posted msg

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,6 +7,11 @@
     
 
     <div class="py-12">
+        @if(session('err_msg'))
+        <p class="text_danger" style="color: blue;">
+            {{session('err_msg')}}
+        </p>
+        @endif
         <!-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8"> -->
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     
