0d0b31144d3a2063f67b96b342271f28d29d26b9
sgmtg <shanbenyan3@gmail.com>
2023-05-06 21:39:13 +0900

    [add] コメントエリア

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,7 @@
         <!-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8"> -->
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     
-                <div class="p-6 text-gray-900">
+                <div class="p-0 text-gray-900">
                     <div class="card">
                         <div class="card-header">
                             Featured
@@ -19,9 +19,22 @@
                             <h6 class="card-title">投稿者：{{$post->user->name}}</h6>
                             <h6 class="card-title">カテゴリー：{{$post->category->category_name}}</h6>
                             <p class="card-text">{{$post->content}}></p>
-                            <!-- <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary">詳細</a> -->
                         </div>
                     </div>
+                </div>
+
+                <div class = "p-6">
+                    <div class="card">
+                  
+                        <div class="card-body">
+                            <!-- <h5 class="card-title">{{$post->title}}</h5>
+                            <h6 class="card-title">投稿者：{{$post->user->name}}</h6>
+                            <h6 class="card-title">カテゴリー：{{$post->category->category_name}}</h6> -->
+                            <p class="card-text">{{$post->content}}></p>
+                        </div>
+                    </div>
+                    <a href="{{ route('posts.show', $post->id) }}" class="btn btn-primary">コメントをする</a>
+
                 </div>
 
                 
