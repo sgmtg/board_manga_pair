eb982a8c4f1c393bad16358f187ada1d2a562aff
sgmtg <shanbenyan3@gmail.com>
2023-05-19 22:08:02 +0900

    [change]

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,7 +12,7 @@
             {{session('err_msg')}}
         </p>
         @endif
-        <!-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8"> -->
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 
                 @foreach($posts as $post)
@@ -56,10 +56,9 @@
                     </div>
                 </div>
                 @endforeach
-                <div style="text-align: center;">
-                    {{ $posts->links() }}
-                </div>
+                {{ $posts->links() }}
             </div>
+        </div>
         <!-- </div> -->
     </div>
     
