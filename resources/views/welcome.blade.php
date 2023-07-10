4c318da04457e60855c606b512633f0260f3771f
sgmtg <shanbenyan3@gmail.com>
2023-07-10 15:28:10 +0900

    add 遷移リンク

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,22 @@
     <div class="relative flex flex-col items-center bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         <div class="p-5">
             <a href="{{ route('posts.index') }}" class="text-3xl font-semibold text-gray-400 hover:text-gray-600 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                掲示版を見る>>
+                全ての投稿を見る>>
+            </a>
+        </div>
+        <div class="p-3">
+            <a href="{{ route('posts.index', ['category_id'=> 2]) }}" class="text-3xl px-4 font-semibold text-green-300 hover:text-green-500 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                原作を探す
+            </a>
+            <a href="{{ route('posts.index', ['category_id'=> 1]) }}" class="text-3xl px-4 font-semibold text-blue-300 hover:text-blue-500 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                作画を探す
             </a>
         </div>
         @if (Route::has('login'))
         <!-- <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">表示位置 -->
         <div class="p-3">
             @auth
-            <a href="{{ url('/dashboard') }}" class="text-3xl p-12 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+            <a href="{{ url('/dashboard') }}" class="text-3xl p-3 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
             @else
             <a href="{{ route('login') }}" class="text-3xl px-4 font-semibold text-green-300 hover:text-green-500 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
             @if (Route::has('register'))
