d9ad8ad98c0fbdcc8542786a49cbad3808a6e7c5
sgmtg <shanbenyan3@gmail.com>
2023-07-15 18:07:29 +0900

    change postのuser_idのnullを許容

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,17 +13,7 @@
 
 
                 <div class = "p-6">
-                    <!-- <div class="card">
-    
-                        <div class="card-body"> -->
-                            <!-- <h5 class="card-title">{{$post->title}}</h5>
-                            <h6 class="card-title">投稿者：{{$post->user->name}}</h6>
-                            <h6 class="card-title">カテゴリー：{{$post->category->category_name}}</h6> -->
-                            <!-- <p class="card-text">{{$post->content}}></p> -->
-                        <!-- </div>
-                    </div> -->
                     <a href="{{ route('comments.create',['post_id'=> $post->id]) }}" class="btn btn-primary" style="background-color:rgba(0,0,0,0.5); font-weight: bold; border:none;">コメントをする</a>
-
                 </div>
                 <div class = "p-6">
                     <h4>コメント一覧</h4>
@@ -31,9 +21,13 @@
                     <div class = "card">
                     <div class="card-body">
                         <h6 class="card-title">投稿者：
-                            <a href = "{{route('users.show', $post->user_id)}}">
-                                {{$comment->user->name}}
-                            </a>
+                        @if($post->user)
+                        <a href="{{route('users.show', $post->user_id)}}" class="font-semibold">
+                            {{$post->user->name}}
+                        </a>
+                        @else
+                            <span class="font-semibold">匿名投稿</span>
+                        @endif
                         </h6>
                         <p class="card-text">{{$comment->comment}}</p>
                     </div>
