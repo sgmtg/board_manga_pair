1bf7f9772f889bf36db7f96a071b1289f30e7e37
sgmtg <shanbenyan3@gmail.com>
2023-07-16 22:40:08 +0900

    change コメント表示

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,37 +6,42 @@
     </x-slot>
 
     <div class="py-12">
-        <!-- <div class="max-w-7xl mx-auto sm:px-6 lg:px-8"> -->
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             @include('posts.post-content')
+            <div class="p-6">
+                <a href="{{ route('comments.create',['post_id'=> $post->id]) }}" class="btn btn-primary" style="background-color:rgba(0,0,0,0.5); font-weight: bold; border:none;">この投稿にコメントをする</a>
+            </div>
+        </div>
+            <div class="p-6">
+                @if(count($post->comments) > 0)
 
+                <h4 class="font-semibold pt-8">コメント一覧</h4>
+                全{{count($post->comments)}}件
 
-                <div class = "p-6">
-                    <a href="{{ route('comments.create',['post_id'=> $post->id]) }}" class="btn btn-primary" style="background-color:rgba(0,0,0,0.5); font-weight: bold; border:none;">コメントをする</a>
-                </div>
-                <div class = "p-6">
-                    <h4>コメント一覧</h4>
-                    @foreach($post->comments as $comment)
-                    <div class = "card">
+                @foreach($post->comments as $comment)
+                <div class="card border-dark">
+                    <div class="card-header flex items-center justify-between font-semibold">
+                        <div>
+                            <span class="">[#{{ $loop->iteration }}]</span>
+                            @if($comment->user)
+                            <a href="{{route('users.show', $comment->user_id)}}" class="font-semibold">
+                                {{$comment->user->name}}
+                            </a>
+                            @else
+                            <span class="font-semibold">non-user</span>
+                            @endif
+                            <span class="text-gray-400">さんからのコメント</span>
+                        </div>
+                        <h6 class="pt-2">{{$post->updated_at}}</h6>
+                    </div>
                     <div class="card-body">
-                        <h6 class="card-title">投稿者：
-                        @if($post->user)
-                        <a href="{{route('users.show', $post->user_id)}}" class="font-semibold">
-                            {{$post->user->name}}
-                        </a>
-                        @else
-                            <span class="font-semibold">匿名投稿</span>
-                        @endif
-                        </h6>
                         <p class="card-text">{{$comment->comment}}</p>
                     </div>
-                    </div>
-                    <br>
-                    @endforeach
                 </div>
-                
+                <br>
+                @endforeach
             </div>
-        <!-- </div> -->
+            @endif
+        </div>
     </div>
 </x-app-layout>
