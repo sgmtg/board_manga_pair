910460070ab23ec04a45d6420e94885221f7886b
sgmtg <shanbenyan3@gmail.com>
2023-07-20 23:57:13 +0900

    add 登録した時に通知 #7

---
+++ app/Notifications/Registered.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class Registered extends Notification
+{
+    use Queueable;
+    public $user;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($val)
+    {
+        //
+        $this->user = $val;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+        ->subject('[原作・作画マッチング掲示板]ユーザ登録ありがとうございます')
+        ->markdown('mail.registered', [
+            'user' => $this->user
+        ]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
